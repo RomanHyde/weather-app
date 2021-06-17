5ace4c1b53bf1ccf5c08931cc1c9634e8587cf7a
RomanHyde <romanhyde1@gmail.com>
2021-06-17 16:20:43 +0100

    typo correction number 2

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const getWeather = require('./lib/getWeather');
 // NOT USING BODY PARSER THAT IS IN THE SLIDES
 const app = express();
 
+const indexRouter = require('./routes/indexRouter');
+const weatherRouter = require('./routes/weatherRouter');
+const errRouter = require('./routes/errRouter');
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -57,6 +61,10 @@ app.post('/weather', async(req,res) => {
 app.get('*', (req, res) => {
     res.render('404');
 });
+
+app.use('/', indexRouter);
+app.use('/weather', weatherRouter);
+app.use('*', errRouter);
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
